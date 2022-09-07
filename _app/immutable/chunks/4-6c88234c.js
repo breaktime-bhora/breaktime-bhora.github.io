a369d0c0557c87667b7193f85d27bb5928c8cb26
SimpleBinary <simplebinary@yahoo.com>
2022-09-07 20:25:28 +0000

    Update gh-pages to output generated at cabd647

---
+++ _app/immutable/chunks/4-6c88234c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/stats/_page.svelte-58c03c70.js";export{t as component};
--- src/routes/stats/+page.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-	<title>Breaktime Bhora</title>
-</svelte:head>
-
-<div class="main">
-	<h1>Coming Soon...</h1>
-</div>

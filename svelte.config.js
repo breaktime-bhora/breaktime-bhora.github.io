cabd647140ee471eb1d6442166e09e1ae5f19ca3
SimpleBinary <simplebinary@yahoo.com>
2022-09-07 22:24:59 +0200

    Update Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    })
+  }
 };
-
-export default config;

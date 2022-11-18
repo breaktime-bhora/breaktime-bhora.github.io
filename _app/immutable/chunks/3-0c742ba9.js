9629d82e2124d6f922c9eed47b3b74c710d7240e
SimpleBinary <simplebinary@yahoo.com>
2022-11-18 19:28:37 +0000

    Update gh-pages to output generated at 32ff433

---
+++ _app/immutable/chunks/3-0c742ba9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/goals/_page.svelte-f6566662.js";export{t as component};
--- _app/immutable/chunks/4-79251273.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/results/_page.svelte-b75089f9.js";export{t as component};
